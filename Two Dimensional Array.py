d84eacfc34bcf39c3d3d2d66787479dd9a7f0f79
Programmer <iramriaz056@gmail.com>
2025-12-29 16:45:39 -0800

    Array

--- Two Dimensional Array.py
+++ Two Dimensional Array.py
@@ -4,7 +4,6 @@
 #Finds the largest and smallest value in a numeric column (e.g., age or marks)
 
 n = int(input("Enter number of records: "))   #Take number of record
-
 table = []         #List to store table data
 
 for i in range(n):      #Input data into the table
@@ -15,12 +14,12 @@ for i in range(n):      #Input data into the table
 
 #Display table
 print("\n--- Table ---")
-print("{:<15} {:<10}".format("Name", "Marks"))
+print("{:<15} {:<10}".format("Name", "Marks"))   # Using String Formatting to Align Name and Marks Columns
 print("-" * 25)
 for row in table:
-    print("{:<15} {:<10}".format(row[0], row[1]))
+    print("{:<15} {:<10}".format(row[0], row[1])) 
 
-marks_list = [row[1] for row in table] #Find max and min marks
+marks_list = [row[1] for row in table]            #Find max and min marks
 max_val = max(marks_list)
 min_val = min(marks_list)
 
